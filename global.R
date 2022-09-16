189a197a838124019c2d75f38aa85897fa08a7d3
TREECE <Nicholas.TREECE@EDUCATION.GOV.UK>
2022-09-16 15:16:59 +0100

    Faffed with functions and shiz

--- global.R
+++ global.R
@@ -82,9 +82,9 @@ source("R/support_links.R")
 source("R/read_data.R")
 
 # Read in the data
-dfRevBal <- read_revenue_data()
+dfProgressHE <- funcReadData('data/data/l4_tidy_2020_all_la.csv')
 # Get geographical levels from data
-dfAreas <- dfRevBal %>%
+dfAreas <- dfProgressHE %>%
   select(
     geographic_level, country_name, country_code,
     region_name, region_code,
@@ -98,11 +98,8 @@ choicesLAs <- dfAreas %>%
   arrange(area_name)
 
 choicesAreas <- dfAreas %>%
-  filter(geographic_level == "National") %>%
-  select(geographic_level, area_name = country_name) %>%
-  rbind(dfAreas %>% filter(geographic_level == "Regional") %>% select(geographic_level, area_name = region_name)) %>%
+  filter(geographic_level == "Regional") %>%
+  select(geographic_level, area_name = region_name) %>%
   rbind(choicesLAs)
-
-choicesYears <- unique(dfRevBal$time_period)
-
-choicesPhase <- unique(dfRevBal$school_phase)
+choicesYears <- unique(dfProgressHE$time_period)
+choicesPhase <- unique(dfProgressHE$data_type)
